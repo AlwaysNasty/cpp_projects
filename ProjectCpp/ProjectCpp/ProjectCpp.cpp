83aafee7515e947458737cf0c6dbfb0b85e4abc5
AlwaysNasty <angrykd00@gmail.com>
2023-02-16 23:50:34 +0300

    add

--- ProjectCpp/ProjectCpp/ProjectCpp.cpp
+++ ProjectCpp/ProjectCpp/ProjectCpp.cpp
@@ -19,6 +19,7 @@
 
 using namespace std;
 
+int fact(int N);
 
 int fact(int N) {
 	if (N == 0)
@@ -33,9 +34,43 @@ int fact(int N) {
 
 int main() {
 	setlocale(LC_ALL, "ru");
-	int N;
-	cin >> N;
-	cout << fact(N);
+	
+    int* arr; // указатель для выделения памяти под массив
+    int size; // размер массива
+    int temp;
+    // Ввод количества элементов массива
+    cout << "n = ";
+    cin >> size;
+
+    if (size <= 0) {
+        // Размер масива должен быть положитлеьным
+        cerr << "Invalid size" << endl;
+        return 1;
+    }
+
+    arr = new int[size]; // выделение памяти под массив
+
+    for (int i = 0; i < size; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < size - 1; i++)
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // меняем элементы местами
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr; // освобождение памяти;
 
 	return 0;
 }
