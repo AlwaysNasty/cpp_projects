1d69c44e02f6f34b89ee28c3633e6d0e8df9ed06
AlwaysNasty <angrykd00@gmail.com>
2023-11-25 00:44:07 +0300

    updated
    added copy of dynamic arrays

--- workWithMemory/Source.cpp
+++ workWithMemory/Source.cpp
@@ -1,6 +1,23 @@
 #include "Header.h"
 
 
+void fillArray( int* const arr,const int size) 
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+
+void readArray(int* const arr, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << arr[i];
+	}
+	std::cout << "\n";
+}
+
 int main() 
 {
 	srand(time(NULL));
@@ -11,18 +28,6 @@ int main()
 
 	delete pa;
 	pa = nullptr;
-
-	int size = 0;
-	std::cin >> size;
-	int* arr = new int[size];
-
-	for (int i = 0; i < size; i++) {
-		arr[i] = rand() % 1000;
-	}
-
-	for (int i = 0; i < size; i++) {
-		std::cout << arr[i]<<"\n";
-	}
 
 	int rows, cols;
 	std::cout << "enter rows and cols: "<<std::endl;
@@ -35,6 +40,8 @@ int main()
 		matrix[i] = new int[cols];
 	}
 	//////////////////////////////////
+
+	//заполнение двумерного динамического массива.
 	for (int i = 0; i < rows; i++) 
 	{
 		for (int j = 0; j < cols; j++) 
@@ -59,7 +66,41 @@ int main()
 		delete[] matrix[i];
 	}
 	delete []matrix;
-	delete []arr;
+	std::cout << "\n";
+	//////////////////////////////////////////////
+
+	int size = 10;
+	int* arr1 = new int[size];
+	int* arr2 = new int[size];
+
+	fillArray(arr1, size);
+	fillArray(arr2, size);
+
+
+	std::cout << "first array: \n";
+	readArray(arr1, size);
+	std::cout << "second array: \n";
+	readArray(arr2, size);
+
+	std::cout << "==================================\n";
+
+	delete[] arr1;//чистим адрес массива, чтобы потом корректно скопировать в него значение второго массива.
+	//если не почистить память массива, то тогда при присваивание значений второго массива первому мы просто присвоим адрес второго массива первому.
+
+	arr1 = new int[size];//заново выделяем память, чтобы заполнить массив 1 значениями массива 2.
+
+	for (int i = 0; i < size; i++)
+	{
+		arr1[i] = arr2[i];
+	}
+
+	std::cout << "first array: \n";
+	readArray(arr1, size);
+	std::cout << "second array: \n";
+	readArray(arr2, size);
+
+	delete[]arr1;
+	delete[]arr2;
 
 	return 0;
 }
