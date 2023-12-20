97e7366fa103e9f476375190fdb18c13de4e5872
AlwaysNasty <angrykd00@gmail.com>
2023-12-20 21:07:20 +0300

    add strRev

---
+++ StrRev/StrRev/StrRev.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char str[] = "This is a test of a programm";
+    char* start, * end;
+    int len;
+    char temp;
+
+    cout << "Start: " << str << "\n";
+
+    len = strlen(str);
+
+    start = str;
+    end = &str[len - 1];
+
+    while (start < end) {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+    cout << "Reversed: " << str << "\n";
+}
+
